c9120c8c753f89a3aa484eab5f754b4b7c53ed5d
ssadras <sadra3tarehdan@gmail.com>
2020-07-16 08:56:00 +0430

    file changed

--- mvc/controller/user.php
+++ mvc/controller/user.php
@@ -1,1 +1,88 @@
 <?php
+class UserController {
+    public function login (){
+        if (isset($_SESSION["user"])){
+            $_SESSION["msg"]=["msg"=>"You are already login.","t_color"=>"white","bg_color"=>"yellow"];
+            header(Domain_R()."home/");
+        }
+        if (!isset($_POST["user"]) and !isset($_POST["pass"])) {
+            $_SESSION["msg"]=["msg"=>"Please complete all of the boxes.","t_color"=>"white","bg_color"=>"red"];
+            $this->loginForm();
+        }else{
+            $this->loginCheck($_POST["user"],$_POST["pass"]);
+        }
+    }
+
+    private function loginCheck($username,$pass){
+        /*
+         * model needed
+         */
+        $db = Db::reader();
+        $result=$db->selectFirstQuery("select * from users where (email=$username or username=$username) and password=$pass");
+        if ($result==null){
+            $_SESSION["msg"]=["msg"=>"Username or password is incorrect","t_color"=>"white","bg_color"=>"red"];
+            $this->loginForm();
+        }
+        else{
+            $_SESSION["user"]=["username"=>$username,"name"=>$result["firstname"]." ".$result["lastname"]];
+            $name=$_SESSION["user"]["name"];
+            $_SESSION["msg"]=["msg"=>"Welcome $name","t_color"=>"white","bg_color"=>"green"];
+            header(Domain_R()."home/");
+        }
+        $db->close();
+        return ;
+    }
+
+    private function loginForm(){
+        
+    }
+
+    public function register (){
+        if (isset($_SESSION["user"])){
+            $_SESSION["msg"]=["msg"=>"You are already login.","t_color"=>"white","bg_color"=>"yellow"];
+            header(Domain_R()."home/");
+        }
+        if (!isset($_POST["username"]) and !isset($_POST["pass"]) and !isset($_POST["email"]) and !isset($_POST["firstname"]) and !isset($_POST["lastname"]) and !isset($_POST["mobile"])) {
+            $_SESSION["msg"]=["msg"=>"Please complete all of the boxes.","t_color"=>"white","bg_color"=>"red"];
+            $this->registerForm();
+        }else{
+            $this->registerCheck($_POST["username"],$_POST["pass"],$_POST["email"],$_POST["firstname"],$_POST["lastname"],$_POST["mobile"]);
+        }
+    }
+
+    private function registerCheck ($username,$pass,$email,$firstname,$lastname,$mobile){
+        $db=Db::reader();
+        $result=$db->selectQuery("select * from users where username=$username or email=$email");
+        if ($result==null){
+            if (!CheckEmail($email)){
+                $_SESSION["msg"]=["msg"=>"Enter a Valid email!","t_color"=>"white","bg_color"=>"red"];
+                $this->registerForm();
+            }elseif (strlen($pass)<8){
+                $_SESSION["msg"]=["msg"=>"Password Must at least 8 characters","t_color"=>"white","bg_color"=>"red"];
+                $this->registerForm();
+            }elseif (strpos($username,"/") or strpos($username,"!") or strpos($username,"-") or strpos($username,"@") or strpos($username,"=") or strpos($username,"\\")){
+                $_SESSION["msg"]=["msg"=>"For your username just use A-Z, a-z, 0-9, underline or dot.","t_color"=>"white","bg_color"=>"red"];
+                $this->registerForm();
+            }else{
+                $pass=md5($pass);
+                if ($db->insertQuery("insert into users (username, password, firstname, lastname, mobile_number, email) values ($username,$pass,$firstname,$lastname,$mobile,$email)")){
+                    $_SESSION["msg"]=["msg"=>"Register completed!","t_color"=>"white","bg_color"=>"green"];
+                    $this->loginForm();
+                }else{
+                    $_SESSION["msg"]=["msg"=>"Something went wrong! please try again!","t_color"=>"white","bg_color"=>"red"];
+                    $this->registerForm();
+                }
+            }
+            $db->close();
+        }
+        else {
+            $_SESSION["msg"]=["msg"=>"your email or username already is in server","t_color"=>"white","bg_color"=>"red"];
+            header(Domain_R()."home/");
+        }
+        return ;
+    }
+
+    private function registerForm(){
+
+    }
+}
