b3ba072e82ca9f785bc59c87e8323d1251cfed89
ssadras <sadra3tarehdan@gmail.com>
2020-07-16 17:41:53 +0430

    Database exported

---
+++ Databases/matrix-7-16-2020.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 16, 2020 at 01:10 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `matrix`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `problemsets`
+--
+
+DROP TABLE IF EXISTS `problemsets`;
+CREATE TABLE IF NOT EXISTS `problemsets` (
+  `problem_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pro_title` varchar(50) NOT NULL,
+  `pro_text` text NOT NULL,
+  `pro_input` varchar(50) NOT NULL,
+  `pro_output` varchar(50) NOT NULL,
+  `pro_test_input` varchar(50) DEFAULT NULL,
+  `pro_test_output` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`problem_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `firstname` varchar(20) DEFAULT NULL,
+  `lastname` varchar(30) DEFAULT NULL,
+  `phone_number` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
