652247c6875c98a42d3ca281fe30e8ed2369d602
Sadra <sadra3tarehdan@gmail.com>
2020-08-13 11:39:07 +0430

    css bug fixed

--- mvc/view/Login.php
+++ mvc/view/Login.php
@@ -3,6 +3,6 @@
 	<input type="text" name="user" id="username" placeholder="  نام کاربری  ">
 	<input type="password" name="pass" id="password" placeholder="   رمز عبور  ">
 	<input onclick="check_empty_value()" type="submit" name="submit" id="submit" value="  ورود  ">
+    <a href="#">  رمز عبور را فراموش کردم  </a>
 </form>
-<a href="#">  رمز عبور را فراموش کردم  </a>
 <script src="../../js/Login.js"></script>
