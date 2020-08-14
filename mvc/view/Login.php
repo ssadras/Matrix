8809b95c4cf11af979c64a6624a310c3065dbba1
Armin <armintm2005.atm@gmail.com>
2020-08-14 18:35:33 +0430

    Login page( it is not responsive yet)

--- mvc/view/Login.php
+++ mvc/view/Login.php
@@ -7,5 +7,6 @@
 	<input type="password" name="pass" id="password" placeholder="   رمز عبور  ">
 	<input onclick="login()" type="button" name="login" id="login" value="  ورود  ">
     <a href="#">  رمز عبور را فراموش کردم  </a>
+    <p> اکانت ندارید ؟ <a href="#">  ثبت نام کنید </a>         </p> 
 </form>
 <script src="../js/Login.js"></script>
