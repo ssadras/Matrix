9cfa8494e0382e8297ccc3353d38ea3cf49c74ad
Arian Noorzadeh <57799214+Arinoor@users.noreply.github.com>
2020-08-14 11:03:34 +0430

    Update Login.php

--- mvc/view/Login.php
+++ mvc/view/Login.php
@@ -2,7 +2,7 @@
 	<h1>  !وارد شوید </h1>
 	<input type="text" name="user" id="username" placeholder="  نام کاربری  ">
 	<input type="password" name="pass" id="password" placeholder="   رمز عبور  ">
-	<input onclick="check_empty_value()" type="button" name="submit" id="submit" value="  ورود  ">
+	<input onclick="submission()" type="button" name="submit" id="submit" value="  ورود  ">
     <a href="#">  رمز عبور را فراموش کردم  </a>
 </form>
 <script src="../../js/Login.js"></script>
