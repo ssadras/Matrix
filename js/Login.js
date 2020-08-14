48274d9eaa6ec8e485f30540d408f86a5ebb2b07
Sadra <sadra3tarehdan@gmail.com>
2020-08-14 11:13:32 +0430

    Jquery Connected to the theme

--- js/Login.js
+++ js/Login.js
@@ -13,16 +13,18 @@ function all_is_number(string) {
 }
 
 function submission(){
-	if (username.value == '' || password.value == '')
+	if (username.value == '' || password.value == '') {
 		alert('salam salam');
 		return false;
-	if (all_is_number(username.value) || all_is_number(password.value))
+	}
+	if (all_is_number(username.value) || all_is_number(password.value)) {
 		alert('چرا همش عدده');
 		return false
+	}
 	$.post('', {
 		'username': username,
 		'password': password,
 		'jsconf': 1 
 	}, next);
 
-}s
+}
