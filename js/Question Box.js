ed66b191f13e7a88deebe50bcb7d6b2fc3cc6b34
Arian Noorzadeh <57799214+Arinoor@users.noreply.github.com>
2020-08-17 23:23:22 +0430

    Update Question Box.js

--- js/Question Box.js
+++ js/Question Box.js
@@ -1,8 +1,15 @@
-var main_box = $('#main_box');
-$.get('../user/login', dataType='json', success=function(data){
-	for (let question_data of data){
-		let new_question = createElement("div");
-		new_question.class = "question_box";
-		let data.createTextNode('name : ' + question_data.name + ' , id : ' + question_data.id + ' , solve_numbers : ' + question_data.solve);
-		new_question.appendChild(data);
-		main_box.appendChild(new_question);
+var main_box = $('#main_box')[0];
+var questions_data = [{name:'Key Kavous', id: '1234', solved:'4'}, {name:'Key Kavous', id: '1234', solved:'4'}, {name:'Key Kavous', id: '1234', solved:'4'}, {name:'Key Kavous', id: '1234', solved:'4'}];
+function show_div(questions_data) {
+	//alert(23);
+	for (let question_data of questions_data){
+		var question_box = document.createElement("div");
+		var paragraph = document.createElement("p");
+		let text = document.createTextNode('name : ' + question_data.name + ' , id : ' + question_data.id + ' solved : ' + question_data.solved);
+		paragraph.appendChild(text);
+		question_box.appendChild(paragraph);
+		question_box.style.color = "blue";
+		main_box.appendChild(question_box);
+	}
+}
+show_div(questions_data);
